9cfbaac50243783da13c402bd35cbbc7d8152ab0
Vichy <andrbowname@yandex.ru>
2024-05-12 17:21:42 -0300

    add basic API for filmorate

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private Long id;
+    @Email
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+}
